b6783f92c647ed10b1885baaca9d041a4ab86a09
Liao Yiyang <liso.yiy@gmail.com>
2016-11-03 16:29:01 +0800

    Update dake.py

--- dake/dake.py
+++ dake/dake.py
@@ -1,29 +1,23 @@
 # -*- coding: utf-8 -*-
 # script for dake
 # requirements: instal splinter and webdriver
-
 from splinter import Browser
 
-#personal info for input
 userName = 'Sxxx'
 psw = '123456'
 
 def dakeIn():
     with Browser('chrome') as browser:
-        url = "https://ckip.worksap.co.jp/cws/cws/srwtimerec"
-        browser.visit(url)
+        browser.visit("https://ckip.worksap.co.jp/cws/cws/srwtimerec")
         browser.fill('user_id', userName)
         browser.fill('password', psw)
-        button = browser.find_by_value(' 出 社 / Work start ')
-        button.click()
+        browser.find_by_value(' 出 社 / Work start ').click()
 
 def dakeOut():
     with Browser('chrome') as browser:
-        url = "https://ckip.worksap.co.jp/cws/cws/srwtimerec"
-        browser.visit(url)
+        browser.visit("https://ckip.worksap.co.jp/cws/cws/srwtimerec")
         browser.fill('user_id', userName)
         browser.fill('password', psw)
-        button = browser.find_by_value(' 退 社 / Work end ')
-        button.click()
+        browser.find_by_value(' 退 社 / Work end ').click()
 
 dakeIn()
