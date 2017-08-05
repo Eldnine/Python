30fd05d775fa89cdea098cd37f0261e8fcb7d8de
Lew <Eldnine@users.noreply.github.com>
2017-08-06 01:16:06 +0800

    Update dake.py

--- dake/dake.py
+++ dake/dake.py
@@ -8,14 +8,14 @@ psw = '123456'
 
 def dakeIn():
     with Browser('chrome') as browser:
-        browser.visit("https://ckip.worksap.co.jp/cws/cws/srwtimerec")
+        browser.visit("***")
         browser.fill('user_id', userName)
         browser.fill('password', psw)
         browser.find_by_value(' 出 社 / Work start ').click()
 
 def dakeOut():
     with Browser('chrome') as browser:
-        browser.visit("https://ckip.worksap.co.jp/cws/cws/srwtimerec")
+        browser.visit("***")
         browser.fill('user_id', userName)
         browser.fill('password', psw)
         browser.find_by_value(' 退 社 / Work end ').click()
